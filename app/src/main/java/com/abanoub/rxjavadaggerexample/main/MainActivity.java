c6621a142c8908e2ff8959ac5ee96f6a0c07d5f9
Abanoub Safwat <abanoub_safwat96@yahoo.com>
2021-08-19 19:54:11 +0200

    retrieving videos and updating UI

--- app/src/main/java/com/abanoub/rxjavadaggerexample/main/MainActivity.java
+++ app/src/main/java/com/abanoub/rxjavadaggerexample/main/MainActivity.java
@@ -1,16 +1,76 @@
 package com.abanoub.rxjavadaggerexample.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.abanoub.rxjavadaggerexample.BuildConfig;
 import com.abanoub.rxjavadaggerexample.R;
+import com.abanoub.rxjavadaggerexample.data.model.Video;
+import com.abanoub.rxjavadaggerexample.di.AppComponent;
+import com.abanoub.rxjavadaggerexample.di.DaggerAppComponent;
+import com.abanoub.rxjavadaggerexample.utils.Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView videosRv;
+    VideosAdapter adapter;
+
+    @Inject
+    MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videosRv = findViewById(R.id.videosRv);
+
+        AppComponent appComponent = DaggerAppComponent.create();
+        appComponent.injectMainActivity(this);
+
+        getVideosFromLocalJsonFile();
+//        viewModel.getVideos();
+
+        viewModel.videosLiveData.observe(this, videos -> {
+            if (videos == null) return;
+
+            adapter = new VideosAdapter(videos, new VideosAdapter.OnItemClick() {
+                @Override
+                public void onDownloadBtnClicked(Video video, int position) {
+
+                }
+            });
+            videosRv.setAdapter(adapter);
+        });
+
+        viewModel.showMsgLiveDate.observe(this, msg -> {
+            if (msg == null) return;
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void getVideosFromLocalJsonFile() {
+        try {
+            InputStream is = getAssets().open("getListOfFilesResponse.json");
+            viewModel.getVideos(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/abanoub/rxjavadaggerexample/main/MainViewModel.java
+++ app/src/main/java/com/abanoub/rxjavadaggerexample/main/MainViewModel.java
@@ -1,6 +1,83 @@
 package com.abanoub.rxjavadaggerexample.main;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.abanoub.rxjavadaggerexample.data.model.Video;
+import com.abanoub.rxjavadaggerexample.data.repository.VideosRepository;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainViewModel extends ViewModel {
+
+    private String TAG = "MainViewModel";
+    MutableLiveData<List<Video>> videosLiveData = new MutableLiveData<List<Video>>();
+    MutableLiveData<String> showMsgLiveDate = new MutableLiveData<>();
+
+    VideosRepository repository;
+
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Inject
+    public MainViewModel(VideosRepository repository) {
+        this.repository = repository;
+    }
+
+    public void getVideos(InputStream is) {
+        videosLiveData.setValue(parseJson(loadJSONFromAsset(is)));
+    }
+
+    public void getVideos() {
+        Observable<List<Video>> observable = repository.getVideos()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        compositeDisposable.add(
+                observable.subscribe(
+                        result -> videosLiveData.setValue((result)),
+                        error -> Log.e(TAG, "getVideos: " + error)));
+    }
+
+    public String loadJSONFromAsset(InputStream is) {
+        String json;
+        try {
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public List<Video> parseJson(String json) {
+        Type reviewType = new TypeToken<List<Video>>() {
+        }.getType();
+
+        List<Video> videos = new Gson().fromJson(json, reviewType);
+        return videos;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.clear();
+    }
 }
