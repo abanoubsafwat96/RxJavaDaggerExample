ec63aadbcc2ec11d822667f389d9b65382b9498f
Abanoub Safwat <abanoub_safwat96@yahoo.com>
2021-08-19 20:22:27 +0200

    enhancing UI and Making item coloring in changing state

--- app/src/main/java/com/abanoub/rxjavadaggerexample/main/VideosAdapter.java
+++ app/src/main/java/com/abanoub/rxjavadaggerexample/main/VideosAdapter.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.abanoub.rxjavadaggerexample.R;
 import com.abanoub.rxjavadaggerexample.data.model.Video;
+import com.google.android.material.card.MaterialCardView;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -38,12 +39,26 @@ public class VideosAdapter extends RecyclerView.Adapter<VideosAdapter.MyViewHold
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         Video item = videosList.get(position);
+        Context context = holder.itemView.getContext();
 
         holder.nameTv.setText(item.getName());
         holder.downloadBtn.setOnClickListener(v -> {
             itemCallback.onDownloadBtnClicked(item, position);
         });
-        holder.progressBar.setProgress(item.getDownloadProgress());
+
+        int downloadProgress = item.getDownloadProgress();
+        holder.progressBar.setProgress(downloadProgress);
+
+        if (downloadProgress == 0)
+            setCardBackgroudColor(holder, context.getResources().getColor(R.color.white));
+        else if (downloadProgress==100)
+            setCardBackgroudColor(holder, context.getResources().getColor(R.color.green));
+        else
+            setCardBackgroudColor(holder, context.getResources().getColor(R.color.yellow));
+    }
+
+    private void setCardBackgroudColor(MyViewHolder holder, int color) {
+        holder.parentCardView.setCardBackgroundColor(color);
     }
 
     @Override
@@ -56,12 +71,14 @@ public class VideosAdapter extends RecyclerView.Adapter<VideosAdapter.MyViewHold
         TextView nameTv;
         Button downloadBtn;
         ProgressBar progressBar;
+        MaterialCardView parentCardView;
 
         public MyViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
             nameTv = itemView.findViewById(R.id.nameTv);
             downloadBtn = itemView.findViewById(R.id.downloadBtn);
             progressBar = itemView.findViewById(R.id.progressBar);
+            parentCardView = itemView.findViewById(R.id.parentCardView);
         }
     }
 
