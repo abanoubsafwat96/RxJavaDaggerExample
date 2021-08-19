2cca5e17ed5d221c7ee823005d55205561bb449d
Abanoub Safwat <abanoub_safwat96@yahoo.com>
2021-08-19 19:30:12 +0200

    Init UI and dependancies

---
+++ app/src/main/java/com/abanoub/rxjavadaggerexample/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.abanoub.rxjavadaggerexample.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abanoub.rxjavadaggerexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
